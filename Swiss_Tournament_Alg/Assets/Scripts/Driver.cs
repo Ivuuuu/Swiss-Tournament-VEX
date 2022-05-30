d04d921d10988d0b0755aabda1012a66b9982507
Ivu <Jasmine.cobb1611@gmail.com>
2022-05-29 23:37:07 -0400

    Generate Matches Implemented + GUI basics

--- Swiss_Tournament_Alg/Assets/Scripts/Driver.cs
+++ Swiss_Tournament_Alg/Assets/Scripts/Driver.cs
@@ -9,9 +9,29 @@ public class Driver : MonoBehaviour
     // VARIABLES
     Team_Template[] teamList;   // holds all team objects
     public Text_Manager textManager;    // refrence to text manager class
+    private int roundNumber = 1;
+    private int matchNumber = 0;
 
-    // read data from a text file, and input information abt the teams
-    void fillTeamArray()
+    private int teamListPlaceHolder = -1;    // number used to navigate through the teamList array
+
+    // the array indexes of the current teams in a match
+    private int red1;
+    private int red2;
+    private int blue1;
+    private int blue2;
+
+    private void Start()
+    {
+        fillTeamArray();    // fill team array (all teams)
+        textManager.displayAllTeams(teamList);
+        Helper_Functions.dumpTeamList(teamList);
+
+        generateFirstRound();
+        textManager.updateRoundNum(roundNumber);
+        Helper_Functions.dumpTeamList(teamList);
+    }
+
+    void fillTeamArray()    // read data from a text file, and input teamNum info
     {
         string path = "Assets/INPUT/TeamList.txt";
         List<string> fileLines = File.ReadLines(path).ToList();
@@ -19,22 +39,12 @@ public class Driver : MonoBehaviour
 
         for (int i = 0; i < fileLines.Count; i++)
         {
-            teamList[i] = new Team_Template();  // fills the team array
+            teamList[i] = new Team_Template();
             teamList[i].teamNum = fileLines[i];
         }
     }
 
-    private void Start()
-    {
-        fillTeamArray();
-        textManager.displayAllTeams(teamList);
-        Helper_Functions.dumpTeamList(teamList);
-        generateFirstRound();
-        Helper_Functions.dumpTeamList(teamList);
-    }
-
-    // randomly generate the first round (bc no data)
-    private void generateFirstRound()
+    private void generateFirstRound()    // randomly generate the first round (bc no data, random matches)
     {
         for (var i = teamList.Length - 1; i > 0; i--)
         {
@@ -44,6 +54,34 @@ public class Driver : MonoBehaviour
             teamList[r] = tmp;
         }
     }
+    /************************************************************
+     * Generating Matches Explained
+     *  say we have teams (in order of strength): A, B, C, D
+     *  to make matches as even as possible we want:
+     *  red alliance: A & D
+     *  blue alliance: B & C
+    *************************************************************/
+    public void generateMatch()    // called when the user hits a button
+    {
+        Debug.Log("button pressed");
+
+        if (teamListPlaceHolder + 4 > teamList.Length - 1)  // if there are not enough teams for the next match, generate new round
+        {
+            Debug.Log("Matches Complete! Starting Next round...");
+            return;
+        }
+
+        matchNumber++;
+
+        red1 = teamListPlaceHolder + 1;
+        blue1 = teamListPlaceHolder + 2;
+        blue2 = teamListPlaceHolder + 3;
+        red2 = teamListPlaceHolder + 4;
+        teamListPlaceHolder += 4;
+
+        textManager.updateMatchNum(matchNumber);
+        textManager.updateMatchText(teamList[red1].teamNum, teamList[red2].teamNum, teamList[blue1].teamNum, teamList[blue2].teamNum);
+    }
 
     // void generateSchedule
     // generate a match for each team, and then display on screen
@@ -51,7 +89,6 @@ public class Driver : MonoBehaviour
     // displayMatches()
     // take 1 & 3, them 2 & 4 to make a match
 
-    // generateFirstRound()
     // displayMatches()
 
     // void displayMatches()
--- Swiss_Tournament_Alg/Assets/Scripts/Text_Manager.cs
+++ Swiss_Tournament_Alg/Assets/Scripts/Text_Manager.cs
@@ -5,7 +5,13 @@ using TMPro;
 
 public class Text_Manager : MonoBehaviour
 {
+    // VARIABLES
     public TextMeshProUGUI teamListText;    // text that displays all team numbers
+    public TextMeshProUGUI roundNum;
+    public TextMeshProUGUI matchNum;
+
+    public TextMeshProUGUI redAlliance;
+    public TextMeshProUGUI blueAlliance;
 
     public void displayAllTeams(Team_Template[] teamList)
     {
@@ -16,5 +22,21 @@ public class Text_Manager : MonoBehaviour
             t += team.teamNum + " ";
         }
         teamListText.text = t;
+    }
+
+    public void updateMatchText(string red1, string red2, string blue1, string blue2)   // updates text on per match basis (like alliances)
+    {
+        redAlliance.text = "Red Alliance" + "\n" + red1 + "\n" + red2;
+        blueAlliance.text = "Blue Alliance" + "\n" + blue1 + "\n" + blue2;
+    }
+
+    public void updateMatchNum(int n)
+    {
+        matchNum.text = "Match# " + n;
+    }
+
+    public void updateRoundNum(int n)
+    {
+        roundNum.text = "Round# " + n;
     }
 }
